f1c0c76b7245c07230884bf1e68a1717a900a3c2
Dahyeon Jin <wlsekgus12@gmail.com>
2024-04-06 18:00:23 +0900

    Feat: 스크랩 저장/취소 구현 완료

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,17 +1,23 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 import supabase from '@/supabaseClient';
 import { Session } from '@supabase/supabase-js';
 
 import { CustomDetailCard } from '../../../components/common/CustomDetailCard';
 
-import { Spacer, Image } from '@nextui-org/react';
-import { useRouter } from 'next/navigation';
+import {
+  Image,
+  Card,
+  CardBody,
+  Textarea,
+  Checkbox,
+  Button,
+} from '@nextui-org/react';
 
-export interface StudyPlace {
+interface StudyPlace {
   id: string;
   place_id: string;
   category: string;
@@ -28,12 +34,16 @@ export interface StudyPlace {
 }
 
 const Detail = () => {
-  const router = useRouter();
-  const pathname = usePathname();
-  console.log('pathname: ', pathname);
-
   const [studyPlace, setStudyPlace] = useState<StudyPlace | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  const [nickname, setNickname] = useState<string | null>(null);
+  const [session, setSession] = useState<Session | null>(null);
+  const [isScrapped, setIsScrapped] = useState(false);
+
+  const router = useRouter();
+
+  const pathname = usePathname();
+  console.log('pathname: ', pathname);
 
   useEffect(() => {
     const fetchStudyPlaceData = async () => {
@@ -66,6 +76,107 @@ const Detail = () => {
     fetchStudyPlaceData();
   }, [pathname]);
 
+  useEffect(() => {
+    const getUserSession = async () => {
+      try {
+        const { data, error } = await supabase.auth.getSession();
+        if (error) throw error;
+        setSession(data.session);
+        console.log('로그인 데이터: ', data);
+      } catch (error) {
+        alert('Session 처리에 오류가 발생했습니다.');
+        console.log(error);
+      }
+    };
+
+    getUserSession();
+  }, [router]);
+
+  useEffect(() => {
+    if (session?.user && typeof session.user.email === 'string') {
+      fetchUserProfile(session.user.email);
+    }
+  }, [session?.user]);
+
+  const fetchUserProfile = async (email: string) => {
+    try {
+      const { data, error } = await supabase
+        .from('user_profiles')
+        .select('nickname')
+        .eq('email', email)
+        .single();
+
+      if (error) {
+        throw error;
+      }
+
+      if (data) {
+        setNickname(data.nickname);
+      }
+    } catch (error) {
+      console.error('사용자 프로필을 불러오는 데 실패했습니다:', error);
+    }
+  };
+
+  // 스터디 장소의 스크랩 여부를 확인하는 함수
+  const checkScrapStatus = async () => {
+    if (session && studyPlace) {
+      const { data, error } = await supabase
+        .from('study_place_scraps')
+        .select('id')
+        .eq('user_id', session.user.id)
+        .eq('study_place_id', studyPlace.id)
+        .single();
+
+      setIsScrapped(!!data);
+    }
+  };
+
+  // 스크랩 추가 함수
+  const addScrap = async () => {
+    if (session && studyPlace && !isScrapped) {
+      const { data, error } = await supabase
+        .from('study_place_scraps')
+        .insert([
+          { user_id: session.user.id, study_place_id: studyPlace.place_id },
+        ]);
+
+      if (error) {
+        console.error('스크랩 추가 실패', error);
+      } else {
+        setIsScrapped(true);
+        console.log('스크랩 추가 성공', data);
+      }
+    }
+  };
+
+  // 스크랩 취소 함수
+  const removeScrap = async () => {
+    if (session && studyPlace && isScrapped) {
+      const { data, error } = await supabase
+        .from('study_place_scraps')
+        .delete()
+        .match({
+          user_id: session.user.id,
+          study_place_id: studyPlace.place_id,
+        });
+
+      console.log('삭제 응답:', data, error);
+
+      if (error) {
+        console.error('스크랩 취소 실패', error);
+      } else {
+        setIsScrapped(false);
+        console.log('스크랩 취소 성공', data);
+      }
+    }
+  };
+
+  // 상태가 변경될 때마다 스크랩 상태 확인
+  useEffect(() => {
+    checkScrapStatus();
+  }, [session, studyPlace]);
+
   if (loading) return <p>Loading...</p>;
   if (!studyPlace) return <p>No study place found</p>;
 
@@ -81,37 +192,97 @@ const Detail = () => {
       </div>
       <div>
         <main className='mx-20 lg:px-8'>
-          <div className='flex items-baseline justify-start pb-2 pt-6'>
-            <h1 className='text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-200'>
-              Categories
-            </h1>
-            <div className='pb-4'>
-              {/* <div className='ml-52 text-2xl font-bold text-gray-700 dark:text-gray-300'>
+          <div className='pb-4'>
+            {/* <div className='ml-52 text-2xl font-bold text-gray-700 dark:text-gray-300'>
                 {nickname
                   ? `${nickname}님이 목표와 꿈을 이루시도록 스플이 함께할게요!`
                   : '3초만에 로그인해서 다양한 서비스를 만나보세요!'}
               </div> */}
-            </div>
           </div>
 
           <section aria-labelledby='products-heading' className='pb-24 pt-6'>
-            <div className='grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4'>
-              <form className='hidden lg:block'>
-                <h3 className='sr-only'>Categories</h3>
-                <div className='space-y-6 pb-8 text-xl font-medium text-gray-900 dark:text-gray-200'>
-                  {/* 여기! */}
+            <div className='grid grid-rows-3 grid-flow-col gap-4'>
+              {/* <form className='hidden lg:block'> */}
+              <div className='row-span-3 space-y-6 pb-8 text-xl font-medium text-gray-900 dark:text-gray-200'>
+                <React.Fragment>
+                  <CustomDetailCard
+                    place={studyPlace}
+                    isScrapped={isScrapped}
+                    onScrap={addScrap}
+                    onUnscrap={removeScrap}
+                  />
+                </React.Fragment>
+              </div>
+              <div>
+                <div className='text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-200'>
+                  No comments
                 </div>
-              </form>
+                <Card>
+                  <CardBody>
+                    <p>
+                      아쉽게도 아직 작성된 리뷰가 없네요! 가장 먼저 리뷰를
+                      달아주세요 :)
+                    </p>
+                  </CardBody>
+                </Card>
+              </div>
 
-              <div className='lg:col-span-3'>
-                <div className='flex flex-wrap'>
-                  <React.Fragment>
-                    <div className='cursor-pointer transform transition duration-300 ease-in-out hover:scale-105'>
-                      <CustomDetailCard place={studyPlace} />
-                    </div>
-                    <Spacer x={4} />
-                  </React.Fragment>
+              <div>
+                <div className='text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-200'>
+                  댓글 쓰기
                 </div>
+                <Card className='w-[480px] p-6'>
+                  <div className='space-y-1 pt-2'>
+                    <div className='text-xl font-bold mb-4'>
+                      {studyPlace.place_name}에 대한 의견을 남겨주세요 :)
+                    </div>
+
+                    <Checkbox defaultSelected radius='sm'>
+                      ⭐️⭐️⭐️⭐️⭐️
+                    </Checkbox>
+
+                    <Textarea
+                      key='faded'
+                      variant='faded'
+                      label='Title'
+                      labelPlacement='outside'
+                      placeholder='Enter the title'
+                      className='col-span-12 md:col-span-6 mb-6 md:mb-0 font-bold'
+                    />
+
+                    <Textarea
+                      key='faded'
+                      variant='faded'
+                      label='Comments'
+                      labelPlacement='outside'
+                      placeholder='Enter the comments'
+                      className='col-span-12 md:col-span-6 mb-6 md:mb-0 font-bold'
+                    />
+                    <Button color='primary' variant='shadow'>
+                      작성하기
+                    </Button>
+                  </div>
+                </Card>
+              </div>
+
+              <div>
+                <div className='text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-200'>
+                  작성된 리뷰
+                </div>
+                <Card className='w-[480px] p-6'>
+                  <div className='space-y-1 pt-2'>
+                    <div className='text-xl font-bold mb-4'>작성된 댓글</div>
+                    <Textarea
+                      isReadOnly
+                      label='Description'
+                      variant='bordered'
+                      labelPlacement='outside'
+                      placeholder='Enter your description'
+                      defaultValue='NextUI is a React UI library that provides a set of accessible, reusable, and beautiful components.'
+                      className='max-w-xs'
+                    />
+                  </div>
+                </Card>
               </div>
             </div>
           </section>
--- src/components/common/CustomDetailCard.jsx
+++ src/components/common/CustomDetailCard.jsx
@@ -1,19 +1,75 @@
 'use client';
 
 import React from 'react';
-import { Card } from '@nextui-org/react';
+import { Card, CardHeader, Button } from '@nextui-org/react';
 
-export const CustomDetailCard = ({ place }) => (
-  <Card className='w-[480px] p-4 mx-2 mb-10' radius='2xl'>
-    <div className='space-y-1 pt-2'>
-      <div className='text-3xl font-bold mb-4'>{place.place_name}</div>
-      <div className='text-lg'>[평점] {place.rating ?? '없음'}</div>
-      <div className='text-lg'>[위치] {place.place_name}</div>
-      <div className='text-lg'>[운영시간] {place.operating_hours}</div>
-      <div className='text-lg'>[연락처] {place.constact}</div>
-      <div className='text-lg'>[이용료] {place.fee ?? '없음'}</div>
-      <div className='text-lg'>[Site URL] {place.website_url ?? '없음'}</div>
-      <div className='text-lg'>[비고] {place.notes ?? '없음'}</div>
-    </div>
-  </Card>
-);
+export const CustomDetailCard = ({ place, isScrapped, onScrap, onUnscrap }) => {
+  const handleScrapClick = () => {
+    if (isScrapped) {
+      onUnscrap();
+    } else {
+      onScrap();
+    }
+  };
+
+  return (
+    <Card className='w-[500px] p-4 mx-2 mb-10' radius='2xl'>
+      <div className='space-y-1 pt-2'>
+        <CardHeader className='justify-between'>
+          <div className='text-4xl font-bold mb-4'>{place.place_name}</div>
+          <Button
+            className={
+              isScrapped
+                ? 'bg-transparent text-foreground border-default-200'
+                : ''
+            }
+            color='primary'
+            radius='full'
+            size='sm'
+            variant={isScrapped ? 'bordered' : 'solid'}
+            onPress={handleScrapClick}
+          >
+            {isScrapped ? (
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='w-6 h-6'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='m3 3 1.664 1.664M21 21l-1.5-1.5m-5.485-1.242L12 17.25 4.5 21V8.742m.164-4.078a2.15 2.15 0 0 1 1.743-1.342 48.507 48.507 0 0 1 11.186 0c1.1.128 1.907 1.077 1.907 2.185V19.5M4.664 4.664 19.5 19.5'
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='w-6 h-6'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z'
+                />
+              </svg>
+            )}
+          </Button>
+        </CardHeader>
+        <div className='text-lg'>[평점] {place.rating ?? '없음'}</div>
+        <div className='text-lg'>[위치] {place.place_name}</div>
+        <div className='text-lg'>[운영시간] {place.operating_hours}</div>
+        <div className='text-lg'>[연락처] {place.contact}</div>
+        <div className='text-lg'>[이용료] {place.fee ?? '없음'}</div>
+        <div className='text-lg'>[Site URL] {place.website_url ?? '없음'}</div>
+        <div className='text-lg'>[비고] {place.notes ?? '없음'}</div>
+      </div>
+    </Card>
+  );
+};
