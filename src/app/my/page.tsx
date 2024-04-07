9398b34ce9711e7f2d959d1af25b117da5a0e1d5
Dahyeon Jin <wlsekgus12@gmail.com>
2024-04-08 02:03:04 +0900

    Feat: 내가 스크랩한 장소 보이는 기능 추가

--- src/app/my/page.tsx
+++ src/app/my/page.tsx
@@ -4,7 +4,10 @@ import React, { useEffect, useState } from 'react';
 import supabase from '@/supabaseClient';
 import { Session } from '@supabase/supabase-js';
 
+import { CustomMainCard } from '../../components/common/CustomMainCard';
 import { useRouter } from 'next/navigation';
+
+import { Card, Spacer } from '@nextui-org/react';
 
 interface UserProfile {
   user_uid: string;
@@ -15,15 +18,33 @@ interface UserProfile {
   user_profile_image: string;
 }
 
+interface StudyPlace {
+  place_id: string;
+  category: string;
+  place_name: string;
+  place_type: string;
+  photo_url: string;
+  rating: number;
+  address: string;
+  operating_hours: string;
+  contact: string;
+  fee: string;
+  website_url: string;
+  notes: string;
+}
+
 const My: React.FC = () => {
-  // const [selectedCategory, setSelectedCategory] = useState<string>('');
+  const [selectedCategory, setSelectedCategory] = useState<string>('profile');
   const [session, setSession] = useState<Session | null>(null);
+
   const [userProfile, setUserProfile] = useState<UserProfile | null>(null);
   const [updatedNickname, setUpdatedNickname] = useState<string>('');
   const [updatedEmail, setUpdatedEmail] = useState<string>('');
   const [updatedUserType, setUpdatedUserType] = useState<string>('');
   const [imageFile, setImageFile] = useState<File | null>(null);
   const [profileImage, setProfileImage] = useState<string>('');
+
+  const [scrappedPlaces, setScrappedPlaces] = useState<StudyPlace[]>([]);
 
   const router = useRouter();
 
@@ -123,62 +144,80 @@ const My: React.FC = () => {
     }
   }, [imageFile]);
 
-  // useEffect(() => {
-  //   fetchStudyPlaces(selectedCategory, selectedPlaceType);
-  // }, [selectedCategory, selectedPlaceType]);
+  const fetchScrappedPlaces = async () => {
+    if (!session?.user?.id) return;
 
-  // const handleCategorySelection = (category: string) => {
-  //   setSelectedCategory(category);
-  //   setSelectedPlaceType('');
-  //   fetchStudyPlaces(category, '');
-  // };
+    const { data: scrappedData, error: scrappedError } = await supabase
+      .from('study_place_scraps')
+      .select('study_place_id')
+      .eq('user_id', session.user.id);
 
-  // const handlePlaceTypeSelection = (placeType: string) => {
-  //   setSelectedPlaceType(placeType);
-  //   setSelectedCategory('');
-  //   fetchStudyPlaces('', placeType);
-  // };
+    if (scrappedError) {
+      console.error('스크랩을 가져오는 중 오류 발생: ', scrappedError);
+      return;
+    }
+
+    const scrappedPlaceIds = scrappedData?.map((scrap) => scrap.study_place_id);
+
+    if (scrappedPlaceIds?.length) {
+      const { data: placesData, error: placesError } = await supabase
+        .from('study_places')
+        .select('*')
+        .in('place_id', scrappedPlaceIds);
+
+      if (placesError) {
+        console.error('장소를 가져오는 중 오류 발생: ', placesError);
+        return;
+      }
+
+      setScrappedPlaces(placesData || []);
+    }
+  };
+
+  useEffect(() => {
+    fetchScrappedPlaces();
+    // 의존성 배열 없어도 될 것 같기도 함
+  }, [session?.user?.id]);
+
+  const handleCategorySelection = (category: string) => {
+    setSelectedCategory(category);
+  };
 
   return (
-    <>
-      <div>
-        <main className='mx-20 lg:px-8'>
-          <div className='flex items-baseline justify-start pb-2 pt-6'>
-            <h1 className='text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-200'>
-              Categories
-            </h1>
-            <div className='pb-4'>
-              <div className='ml-52 text-2xl font-bold text-gray-700 dark:text-gray-300'>
-                {userProfile?.nickname}님의 프로필 정보
-              </div>
+    <div>
+      <main className='mx-20 lg:px-8'>
+        <div className='flex items-baseline justify-start pb-2 pt-6'>
+          <h1 className='text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-200'>
+            Categories
+          </h1>
+          <div className='pb-4'>
+            <div className='ml-52 text-2xl font-bold text-gray-700 dark:text-gray-300'>
+              {userProfile?.nickname}님의 프로필 정보
             </div>
           </div>
+        </div>
 
-          <section aria-labelledby='products-heading' className='pb-24 pt-6'>
+        {selectedCategory === 'profile' && (
+          <div aria-labelledby='products-heading' className='pb-24 pt-6'>
             <div className='grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4'>
               <div className='lg:col-span-1 mt-2 space-y-6 pb-8 text-xl font-medium text-gray-900 dark:text-gray-200'>
                 <div>
-                  <button
-                  // onClick={() => handleCategorySelection('')}
-                  >
+                  <button onClick={() => handleCategorySelection('profile')}>
                     내 프로필 정보
                   </button>
                 </div>
                 <div>
-                  <button
-                  // onClick={() => handlePlaceTypeSelection('스터디룸')}
-                  >
+                  <button onClick={() => handleCategorySelection('scrapped')}>
                     내가 스크랩한 장소
                   </button>
                 </div>
                 <div>
-                  <button
-                  // onClick={() => handlePlaceTypeSelection('스터디카페')}
-                  >
+                  <button onClick={() => handleCategorySelection('comments')}>
                     내가 작성한 댓글
                   </button>
                 </div>
               </div>
+
               <div className='lg:col-span-1'>
                 <div className='w-full overflow-hidden mb-8'>
                   <div
@@ -203,6 +242,7 @@ const My: React.FC = () => {
                   이미지 변경하기
                 </button>
               </div>
+
               <div className='lg:col-span-2'>
                 <div className='w-full grid grid-cols-1 md:grid-cols-2 gap-4 flex flex-wrap'>
                   <label className='text-2xl font-bold ml-3'>닉네임</label>
@@ -237,10 +277,152 @@ const My: React.FC = () => {
                 </button>
               </div>
             </div>
-          </section>
-        </main>
-      </div>
-    </>
+          </div>
+        )}
+
+        {selectedCategory === 'scrapped' && (
+          <div aria-labelledby='products-heading' className='pb-24 pt-6'>
+            <div className='grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4'>
+              <div className='lg:col-span-1 mt-2 space-y-6 pb-8 text-xl font-medium text-gray-900 dark:text-gray-200'>
+                <div>
+                  <button onClick={() => handleCategorySelection('profile')}>
+                    내 프로필 정보
+                  </button>
+                </div>
+                <div>
+                  <button onClick={() => handleCategorySelection('scrapped')}>
+                    내가 스크랩한 장소
+                  </button>
+                </div>
+                <div>
+                  <button onClick={() => handleCategorySelection('comments')}>
+                    내가 작성한 댓글
+                  </button>
+                </div>
+              </div>
+
+              <div className='lg:col-span-3'>
+                {scrappedPlaces.length === 0 ? (
+                  <Card className='flex justify-center items-center h-full w-full max-w-[430px]'>
+                    <div className='flex flex-col justify-center items-center py-10 space-y-1 py-2 mx-2'>
+                      <div>
+                        <p className='text-xl font-bold'>
+                          스크랩하신 장소가 없습니다.
+                        </p>
+                      </div>
+                    </div>
+                  </Card>
+                ) : (
+                  <div className='flex flex-wrap'>
+                    {scrappedPlaces.map((place) => (
+                      <React.Fragment key={place.place_id}>
+                        <div
+                          className='cursor-pointer transform transition duration-300 ease-in-out hover:scale-105'
+                          onClick={() =>
+                            router.push(`/detail/${place.place_id}`)
+                          }
+                        >
+                          <CustomMainCard
+                            place={place}
+                            onCardClick={(id: string) =>
+                              router.push(`/detail/${id}`)
+                            }
+                          />
+                        </div>
+                        <Spacer x={4} />
+                      </React.Fragment>
+                    ))}
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        )}
+
+        {selectedCategory === 'comments' && (
+          <div aria-labelledby='products-heading' className='pb-24 pt-6'>
+            <div className='grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4'>
+              <div className='lg:col-span-1 mt-2 space-y-6 pb-8 text-xl font-medium text-gray-900 dark:text-gray-200'>
+                <div>
+                  <button onClick={() => handleCategorySelection('profile')}>
+                    내 프로필 정보
+                  </button>
+                </div>
+                <div>
+                  <button onClick={() => handleCategorySelection('scrapped')}>
+                    내가 스크랩한 장소
+                  </button>
+                </div>
+                <div>
+                  <button onClick={() => handleCategorySelection('comments')}>
+                    내가 작성한 댓글
+                  </button>
+                </div>
+              </div>
+
+              <div className='lg:col-span-1'>
+                <div className='w-full overflow-hidden mb-8'>
+                  <div
+                    className='rounded-lg bg-cover bg-center bg-no-repeat w-[300px] h-[300px] p-4'
+                    style={{
+                      backgroundImage: `url(${
+                        profileImage || '/default-profile.png'
+                      })`,
+                    }}
+                  ></div>
+                </div>
+                <input
+                  className='mt-1 block w-[300px] px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500'
+                  name='user_profile_image'
+                  type='file'
+                  onChange={handleImageChange}
+                />
+                <button
+                  className=' w-[300px] mt-2 px-4 py-2 bg-blue-500 text-white rounded-md'
+                  onClick={() => imageFile && uploadProfileImage()}
+                >
+                  이미지 변경하기
+                </button>
+              </div>
+
+              <div className='lg:col-span-2'>
+                <div className='w-full grid grid-cols-1 md:grid-cols-2 gap-4 flex flex-wrap'>
+                  <label className='text-2xl font-bold ml-3'>닉네임</label>
+                  <input
+                    className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500'
+                    name='nickname'
+                    value={updatedNickname}
+                    onChange={(e) => setUpdatedNickname(e.target.value)}
+                  />
+                  <label className='text-2xl font-bold ml-3'>이메일</label>
+                  <input
+                    className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500'
+                    name='email'
+                    value={updatedEmail}
+                    onChange={(e) => setUpdatedEmail(e.target.value)}
+                  />
+                  <label className='text-2xl font-bold ml-3'>유형</label>
+                  <input
+                    className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500'
+                    name='user_type'
+                    value={updatedUserType}
+                    onChange={(e) => setUpdatedUserType(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className='lg:col-span-4 flex justify-end'>
+                <button
+                  className=' w-[250px] my-4 px-4 py-2 bg-blue-500 text-white rounded-md'
+                  onClick={updateUserProfile}
+                >
+                  프로필 저장하기
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </main>
+    </div>
   );
 };
 
