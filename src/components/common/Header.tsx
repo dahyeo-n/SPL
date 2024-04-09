d1f0bf46ebb2b5abe25268da21f6d017de452137
Dahyeon Jin <wlsekgus12@gmail.com>
2024-04-10 05:58:26 +0900

    Style: Header image 크기 변경

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Image } from '@nextui-org/react';
 
 const Header = () => {
   return (
     <div className='flex justify-center w-full overflow-hidden mb-8'>
-      <Image
-        width={500}
-        height={200}
-        alt='NextUI hero Image with delay'
-        src='https://app.requestly.io/delay/5000/https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg'
-      />
+      <div
+        className='rounded-lg bg-cover bg-center bg-no-repeat w-[1000px] h-[400px] p-4 mx-2'
+        style={{
+          backgroundImage: `url(https://app.requestly.io/delay/5000/https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg)`,
+        }}
+      ></div>
     </div>
   );
 };
