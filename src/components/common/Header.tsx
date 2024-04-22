b28e40f0a24d934ad2aa77066fb0b158be9ae62a
Dahyeon Jin <wlsekgus12@gmail.com>
2024-04-22 23:23:41 +0900

    Feat: SPL 설문 버튼 생성

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import EmblaCarousel from '../common/EmblaCarousel';
 import { EmblaOptionsType } from 'embla-carousel';
+import Link from 'next/link';
 
 const OPTIONS: EmblaOptionsType = { loop: true };
-const SLIDES = [
-  '/images/HeaderImg.jpg',
-  '/images/HeaderImg.jpg',
-  '/images/HeaderImg.jpg',
-  '/images/HeaderImg.jpg',
-  '/images/HeaderImg.jpg',
-];
+const SLIDES = ['/images/HeaderImg.jpg', '/images/spl-survey.jpg'];
 
 const Header: React.FC = () => {
   return (
-    <div className='flex justify-center w-full overflow-hidden mb-2'>
-      {/* <div
-        className='rounded-lg bg-cover bg-center bg-no-repeat w-[1000px] h-[400px] p-4 mx-2'
-        style={{
-          backgroundImage: `url('/images/HeaderImg.jpg')`,
-        }}
-      ></div> */}
+    <div className='relative flex justify-center w-full overflow-hidden mb-2'>
       <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+      {SLIDES.map(
+        (slide, index) =>
+          slide === '/images/spl-survey.jpg' && (
+            <Link
+              key={index}
+              href='https://forms.gle/WfVJzgWfBbMqUexo6'
+              className='absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white text-black px-4 py-2 rounded-md'
+              style={{ zIndex: 10 }}
+            >
+              SPL 설문 시작하기
+            </Link>
+          )
+      )}
     </div>
   );
 };
